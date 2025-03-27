5ee8c7680f2091d29423e969dcd7e9245b1d9f1f
jeffspoolspa <carter@jeffspoolspa.com>
2025-03-26 22:56:35 -0400

    Update price_finder.py

--- price_finder.py
+++ price_finder.py
@@ -43,7 +43,7 @@ def get_price(part_number):
 
     # Connect to the running Selenium server
     driver = webdriver.Remote(
-        command_executor='http://localhost:4444/wd/hub',
+        command_executor='http://selenium:4444/wd/hub',
         options=options
     )
 
